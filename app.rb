25df34e4570920f930aa8a857f125e30c81fb536
MarvinProg <85562528+MarvinProg@users.noreply.github.com>
2021-06-11 20:18:03 +0300

    Init barbers table on configure

--- app.rb
+++ app.rb
@@ -4,6 +4,20 @@ require 'sinatra/reloader'
 require "sqlite3"
 
 
+def is_barber_exist? db, name
+  db.execute('select* from Barbers where name=?', [name]).length > 0
+end
+
+def seed_db db, barbers
+
+  barbers.each do |barber|
+    if is_barber_exist? db, barber
+        db.execute 'insert into Barbers (name) values (?)', [barber]
+    end
+  end
+end
+
+
 def get_db
   return SQLite3::Database.new 'barbershop.db'
   db.results_as_hash = true
@@ -22,6 +36,16 @@ configure do
       "barber" TEXT,
       "color" TEXT
     )'
+
+  db.execute 'CREATE TABLE IF NOT EXISTS 
+    "Barbers" 
+    (
+      "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+      "name" TEXT,
+    )'
+
+    seed_db db, [Иван, Наталья, Джек руки-ножницы, другой]
+
 end
 
 get '/about' do
